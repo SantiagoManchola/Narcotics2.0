adffe59246ce6e686fc9672410dc1aeb7f9d5553
SantiagoManchola <dmancholas@hotmail.com>
2024-05-13 12:17:26 -0500

    Cambios Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 const header = document.getElementById("header");
+const nextBtn = document.getElementById("right");
+const prevBtn = document.getElementById("left");
 const sectionNew = document.querySelectorAll(".sectionNew");
 
 // Function to apply classes based on the scroll
@@ -13,6 +15,33 @@ function isScrolled(value) {
 window.addEventListener("scroll", () => {
   isScrolled(0);
 });
+
+let currentIndex = 0;
+
+nextBtn.addEventListener('click', () => {
+    if (currentIndex < sectionNew.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; // Vuelve al primer elemento si llega al final
+    }
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = sectionNew.length - 1; // Va al último elemento si está en el primero
+    }
+    updateCarousel();
+});
+
+function updateCarousel() {
+    const newPosition = -currentIndex * 100;
+    sectionNew.forEach((e) => {
+        e.style.transform = `translateX(${newPosition}%)`;
+    });
+}
 
 $( document ).ready( function() {
   var images = [
@@ -140,3 +169,4 @@ var scene = new ScrollMagic.Scene({triggerElement: "#trigger-2", duration: "1200
         .addIndicators()
         .addTo(controller);
 })
+
